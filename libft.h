9ad36ae5255f3ae12ba01bbc6604281c8abbafb5
Civan Rumet Arı <urmet@Civan-MacBook-Air.local>
2024-11-03 01:57:01 +0300

    ft_putendl_fd.c added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: urmet <urmet@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 14:27:24 by tuaydin           #+#    #+#             */
-/*   Updated: 2024/11/03 01:37:12 by urmet            ###   ########.fr       */
+/*   Updated: 2024/11/03 01:38:55 by urmet            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
